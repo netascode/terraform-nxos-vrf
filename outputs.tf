e3ef19be5505a60608743e515c7cc25fc78cc84a
Ivan Sinyagovskiy <vaneuk@gmail.com>
2022-04-18 23:43:40 +0300

    changed outputs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "dn" {
-  value       = "sys/inst-${var.name}"
+  value       = var.name == "default" ? "sys/inst-${var.name}" : nxos_vrf.l3Inst[0].id
   description = "Distinguished name of the object."
 }
 
 output "name" {
-  value       = var.name
+  value       = var.name == "default" ? var.name : nxos_vrf.l3Inst[0].name
   description = "VRF name."
 }
